3fa40bb2333fd38e9d81b5d74cf6409d5198db90
abdulsharif <abdulsharif@localhost>
2023-01-12 19:23:25 -0500

    this is another comment we add more method

---
+++ src/com/noorteck/git/day2/DemoDay2.java
@@ -0,0 +1,12 @@
+package com.noorteck.git.day2;
+
+public class DemoDay2 {
+	
+	public static void main(String[] args) {
+		System.out.println("Day two practice gitHub");
+		
+	}
+public static void  addTwoPush() {
+	
+}
+}
